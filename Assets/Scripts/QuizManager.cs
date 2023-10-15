31646254cb04ec66adfb278e98d70c1726ac1fe1
MarlonY123 <A01424875@tec.mx>
2023-10-14 19:08:11 -0600

    Audio Manager

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,52 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-
+using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class QuizManager : MonoBehaviour
 {
-    public List<QuestionAnswer> QnA;
+    public static QuizManager instance;
 
+
+    public List<QuestionsAndAnswers> QnA;
     public GameObject[] options;
     public int currentQuestion;
 
     public TextMeshProUGUI QuestionTxt;
 
+    public int totalQuestions = 0;
+
+    ScoreSystem scoreSystem;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            instance.SetManagers();
+        }
+        else
+        {
+            instance = this;
+            SetManagers();
+        }
+
+    }
+
     private void Start()
     {
+        totalQuestions = QnA.Count;
         generateQuestion();
+    }
+
+    public void SetManagers()
+    {
+        if (scoreSystem == null)
+        {
+            ScoreSystem scoreSystemInstance = FindObjectOfType<ScoreSystem>();
+            scoreSystem = scoreSystemInstance;
+        }
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Puntuacion.preguntasTot = totalQuestions;
+
     }
 
     public void correct()
     {
-
+        scoreSystem.score += 1;
         QnA.RemoveAt(currentQuestion);
         generateQuestion();
     }
+
+    public void wrong()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
     void SetAnswer()
     {
         for (int i = 0; i < options.Length; i++)
         {
-            options[i].GetComponent<AnswerScript>().isCorrect = false;
-            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text =  QnA[currentQuestion].Answers[i];
-        
-            if(QnA[currentQuestion].CorrectAnswer == i+1)
+            options[i].GetComponent<AnswersManager>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QnA[currentQuestion].Answers[i];
+
+            if (QnA[currentQuestion].correctAnswer == i + 1)
             {
-                options[i].GetComponent<AnswerScript>().isCorrect = true;
+                options[i].GetComponent<AnswersManager>().isCorrect = true;
             }
         }
     }
+
     void generateQuestion()
     {
-        currentQuestion = Random.Range(0, QnA.Count);
+        if (QnA.Count > 0)
+        {
+            currentQuestion = Random.Range(0, QnA.Count);
 
-        QuestionTxt.text = QnA[currentQuestion].Question;
-        SetAnswer();
-
+            QuestionTxt.text = QnA[currentQuestion].Question;
+            SetAnswer();
+        }
+        else
+        {
+            Debug.Log("Out of Questions");
+            GameOver();
+        }
         
     }
-
-    
 }
---
+++ Assets/Scripts/SceneMan.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMan : MonoBehaviour
+{
+
+    [SerializeField] private ExitWarningConfirmation myExitWarningConfirmation;
+    
+
+    public void mainMenue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quizScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void salirIntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    public void salirMenuPrincipal()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void salirJuego()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
+
+
+
+    public void confirmationWindow()
+    {
+        openConfirmationWindow();
+    }
+
+    private void openConfirmationWindow()
+    {
+        myExitWarningConfirmation.gameObject.SetActive(true);
+        myExitWarningConfirmation.yesButton.onClick.AddListener(YesClicked);
+        myExitWarningConfirmation.noButton.onClick.AddListener(NoClicked);
+    }
+
+    private void YesClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    private void NoClicked()
+    {
+        myExitWarningConfirmation.gameObject.SetActive(false);
+    }
+
+
+}
